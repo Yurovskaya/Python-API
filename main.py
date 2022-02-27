0ee8774992c0efb7654020959d2467819b831991
Yurovskaya <kontrust131@gmail.com>
2022-02-27 18:38:21 +0300

    status

--- main.py
+++ main.py
@@ -39,4 +39,37 @@ print(response_2.text)
 response_4 = requests.post("https://playground.learnqa.ru/api/check_type",data= {"name": "Dasha"})
 print(response_4.text)
 
+# Коды ответов от сервера
+
+response_5 = requests.get("https://playground.learnqa.ru/api/check_type")
+print(response_5.status_code)
+
+# Статус код 500
+# Выводим и код ответа и текст
+
+response_6 = requests.get("https://playground.learnqa.ru/api/get_500")
+print(response_6.status_code)
+print(response_6.text)
+
+# Статус код 404
+
+response_7 = requests.get("https://playground.learnqa.ru/api/something")
+print(response_7.status_code)
+print(response_7.text)
+
+# Статус код 301 Moved Permanently (переехал навсегда)
+# Статус код 302 Moved Temporarily (временно перемещен)
+response_8 = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+print(response_8.status_code)
+
+# Чтобы узнать куда именно нас перенаправили
+response_9 = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+first_response = response_9.history[0]
+second_response = response_9
+
+print(first_response.url)
+print(second_response.url)
+
+
+
 
