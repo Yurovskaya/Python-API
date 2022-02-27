82def138f1a965257479c42ee7f324e7f6595e4f
Yurovskaya <kontrust131@gmail.com>
2022-02-27 17:16:58 +0300

    ghgh

---
+++ Json_parsing.py
@@ -0,0 +1,14 @@
+# Парсинг json
+
+import json
+
+string_as_json_format = '{"answer": "Hello, Dasha"}'
+obj = json.loads(string_as_json_format)
+key = "answer2"
+
+if key in obj:
+    print(obj[key])
+else:
+    print(f"Ключа {key} в JSON нет")
+
+
--- main.py
+++ main.py
@@ -3,11 +3,26 @@ import requests
 response = requests.get("https://playground.learnqa.ru/api/hello")
 print(response.text)
 
+# Передача параметра в запросе.
+payload = {"name": "Dasha"}
+response_1 = requests.get("https://playground.learnqa.ru/api/hello",params=payload)
+print(response_1.text)
+
 # Ex4: GET-запрос
 #
 # В проекте создать скрипт, который отправляет GET-запрос по адресу: https://playground.learnqa.ru/api/get_text
 # Затем с помощью функции print вывести содержимое текста в ответе на запрос.
 # Когда скрипт будет готов - давайте его закоммитим в наш репозиторий.
 
-response_one = requests.get ("https://playground.learnqa.ru/api/get_text")
+response_one = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response_one.text)
+
+# Парсим полученный ответ
+
+
+
+
+
+
+
+
